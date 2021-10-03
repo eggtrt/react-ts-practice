c4f2f15ded1d6449388f58d19f66bfd8caca61a3
eggtrt <eggtrt@gmail.com>
2021-10-03 14:51:00 +0900

    dispatch

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Greetings from './Greeting';
+import ReducerSample from './ReducerSample';
+import { SampleProvider } from './SampleContext';
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-  return <Greetings name="Hello" onClick={onClick} />;
+  return (
+    <SampleProvider>
+      <ReducerSample />
+    </SampleProvider>
+  );
 };
 
 export default App;
